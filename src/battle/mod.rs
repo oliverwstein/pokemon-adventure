2002bf9fcbcf68f6b0aa3b48284e91ef39d62154
oliverwstein <oliversteininc@gmail.com>
2025-08-11 14:40:39 -0400

    Moved all the battle test files into a subfolder

--- src/battle/mod.rs
+++ src/battle/mod.rs
@@ -3,28 +3,4 @@ pub mod stats;
 pub mod turn_orchestrator;
 
 #[cfg(test)]
-mod test_resolve_turn;
-
-#[cfg(test)]
-mod test_critical_hits;
-
-#[cfg(test)]
-mod test_fainting;
-
-#[cfg(test)]
-mod test_multi_attacks;
-
-#[cfg(test)]
-mod test_end_of_turn;
-
-#[cfg(test)]
-mod test_action_prevention;
-
-#[cfg(test)]
-mod test_status_moves;
-
-#[cfg(test)]
-mod test_damage_effects;
-
-#[cfg(test)]
-mod test_condition_damage;
+mod tests;
---
+++ src/battle/tests/mod.rs
@@ -0,0 +1,26 @@
+#[cfg(test)]
+mod test_resolve_turn;
+
+#[cfg(test)]
+mod test_critical_hits;
+
+#[cfg(test)]
+mod test_fainting;
+
+#[cfg(test)]
+mod test_multi_attacks;
+
+#[cfg(test)]
+mod test_end_of_turn;
+
+#[cfg(test)]
+mod test_action_prevention;
+
+#[cfg(test)]
+mod test_status_moves;
+
+#[cfg(test)]
+mod test_damage_effects;
+
+#[cfg(test)]
+mod test_condition_damage;

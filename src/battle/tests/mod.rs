00ba1748530bf05b2a23a1ebc2d8f9a17811e0af
oliverwstein <oliversteininc@gmail.com>
2025-08-12 15:11:58 -0400

    removed unnecessary #[cfg(test)] lines.

--- src/battle/tests/mod.rs
+++ src/battle/tests/mod.rs
@@ -1,62 +1,21 @@
-#[cfg(test)]
 mod test_resolve_turn;
-
-#[cfg(test)]
 mod test_critical_hits;
-
-#[cfg(test)]
 mod test_fainting;
-
-#[cfg(test)]
 mod test_multi_attacks;
-
-#[cfg(test)]
 mod test_end_of_turn;
-
-#[cfg(test)]
 mod test_action_prevention;
-
-#[cfg(test)]
 mod test_status_moves;
-
-#[cfg(test)]
 mod test_damage_effects;
-
-#[cfg(test)]
 mod test_condition_damage;
-
-#[cfg(test)]
 mod test_special_moves;
-
-#[cfg(test)]
 mod test_nightmare;
-
-#[cfg(test)]
 mod test_heal;
-
-#[cfg(test)]
 mod test_haze;
-
-#[cfg(test)]
 mod test_cure_status;
-
-#[cfg(test)]
 mod test_reckless;
-
-#[cfg(test)]
 mod test_reflect_lightscreen;
-
-#[cfg(test)]
 mod test_team_condition_expiry;
-
-#[cfg(test)]
 mod test_mist;
-
-#[cfg(test)]
 mod test_team_condition_moves;
-
-#[cfg(test)]
 mod test_ante;
-
-#[cfg(test)]
 mod test_metronome;
